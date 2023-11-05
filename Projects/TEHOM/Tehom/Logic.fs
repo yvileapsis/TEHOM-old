d47bd456fafd0e86127266e6bcc86dd4af979e6d
yvileapsis <yvileapsis@gmail.com>
2023-11-05 14:57:41 +0300

    WIP

---
+++ Projects/TEHOM/Tehom/Logic.fs
@@ -0,0 +1,28 @@
+﻿module Logic
+
+//
+// --------- Logic --------- 
+//
+
+type Result<'TSuccess, 'TFailure> =
+    | Success of 'TSuccess
+    | Failure of 'TFailure
+
+let bind processFunc lastResult =
+    match lastResult with
+    | Success s -> processFunc s
+    | Failure f -> Failure f
+
+let (>>=) x f =
+    bind f x
+
+let foldBackBind processFunc set =
+    let inner world =
+        let folder result object =
+            result >>= processFunc object
+
+        set |> List.fold folder (Success world)
+    inner
+
+let switch processFunc input =
+    Success (processFunc input)
